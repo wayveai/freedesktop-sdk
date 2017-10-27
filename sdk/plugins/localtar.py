420f149c606bd40f60f9ce8db56e88c8b19afea5
Valentin David <valentin.david@gmail.com>
2017-10-27 02:12:19 +0200

    Add base sdk

---
+++ sdk/plugins/localtar.py
@@ -0,0 +1,45 @@
+import os
+import tarfile
+
+from buildstream import Source, SourceError, Consistency
+from buildstream import utils
+
+
+class LocalTarSource(Source):
+
+    def configure(self, node):
+        project = self.get_project()
+
+        self.node_validate(node, ['path'] + Source.COMMON_CONFIG_KEYS)
+        self.path = self.node_get_member(node, str, 'path')
+        self.fullpath = os.path.join(project.directory, self.path)
+
+    def preflight(self):
+        if not os.path.exists(self.fullpath):
+            raise SourceError("Specified path '%s' does not exist" % self.path)
+
+    def get_unique_key(self):
+        return utils.sha256sum(self.fullpath)
+
+    def get_consistency(self):
+        return Consistency.CACHED
+
+    def get_ref(self):
+        return None
+
+    def set_ref(self, ref, node):
+        pass
+
+    def fetch(self):
+        pass
+
+    def stage(self, directory):
+        try:
+            with tarfile.open(self.fullpath) as tar:
+                tar.extractall(path=directory)
+
+        except (tarfile.TarError, OSError) as e:
+            raise SourceError("{}: Error staging source: {}".format(self, e)) from e
+
+def setup():
+    return LocalTarSource
