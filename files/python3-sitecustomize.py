c32afc8e53ca7dd6a7a8903cd3e2d6837998f16e
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2019-04-05 23:42:19 +0300

    Do some surgery to sys.path sitecustomize is imported only after site has already reconfigured sys.path so we need to do what is actually intended, it USER_SITE needs to be removed and then re-added to same index. Order matters!

--- files/python3-sitecustomize.py
+++ files/python3-sitecustomize.py
@@ -1,4 +1,10 @@
+import sys
 import site
 import os
 
 site.USER_BASE = os.environ.get("PYTHONUSERBASE", "/var/data/python")
+old_index = sys.path.index(site.USER_SITE)
+sys.path.remove(site.USER_SITE)
+site.USER_SITE = None
+
+sys.path.insert(old_index, site.getusersitepackages())
