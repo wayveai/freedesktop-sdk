wayveai/freedesktop-sdk

commit 17a43122d5178ac3e2f4283adf7b2f9e8d6cda2f

apply shellcheck recommended fixed
- use globs instead of ls since it's fragile
- quote vars to prevent globbing and word splitting
- make scripts and shebangs properly match

utils/update-repo.sh
@@ -17,7 +17,7 @@ while [ $# -gt 0 ]; do
 	    gpg_opts+=("$1")
 	    ;;
 	--gpg-*)
-	    gpg_opts+=("$1", "$2")
+	    gpg_opts+=("$1" "$2")
 	    shift
 	    ;;
 	--collection-id=*)
@@ -40,7 +40,7 @@ while [ $# -gt 0 ]; do
 	    exit 1
 	    ;;
 	-*)
-	    for ((i=1;$i < ${#1};++i)); do
+	    for ((i=1;i < ${#1};++i)); do
 		case "${1:i}" in
 		    h)
 			help
@@ -70,7 +70,7 @@ export OSTREE_REPO
 element="${main_opts[1]}"
 ref="${main_opts[2]}"
 
-checkout="$(mktemp --suffix="-update-repo" -d -p "$(dirname ${OSTREE_REPO})")"
+checkout="$(mktemp --suffix="-update-repo" -d -p "$(dirname "${OSTREE_REPO}")")"
 
 on_exit() {
     rm -rf "${checkout}"
@@ -80,8 +80,8 @@ trap on_exit EXIT
 ${BST:-bst} build "${element}"
 ${BST:-bst} checkout --hardlinks "${element}" "${checkout}"
 
-if ! [ -d ${OSTREE_REPO} ]; then
-    ostree init --repo=${OSTREE_REPO} --mode=archive
+if ! [ -d "${OSTREE_REPO}" ]; then
+    ostree init --repo="${OSTREE_REPO}" --mode=archive
 fi
 
 commit="$(ostree --repo="${checkout}/ostree/repo" rev-parse "${ref}")"
@@ -89,7 +89,7 @@ ostree pull-local "${checkout}/ostree/repo" "${commit}"
 
 prev_commit="$(ostree rev-parse "${ref}" 2>/dev/null || true)"
 
-ostree commit ${gpg_opts[*]} \
+ostree commit "${gpg_opts[*]}" \
        --branch="${ref}" --tree=ref="${commit}" --skip-if-unchanged
 
 new_commit="$(ostree rev-parse "${ref}")"
@@ -103,6 +103,6 @@ if [ "${new_commit}" != "${prev_commit}" ]; then
 
     ostree summary \
            ${collection_id:+--add-metadata=ostree.deploy-collection-id='"'"${collection_id}"'"'} \
-           ${gpg_opts[*]} \
+           "${gpg_opts[*]}" \
            --update
 fi

files/vm/prepare-image.sh
@@ -80,7 +80,8 @@ cat <<EOF >"${sysroot}/etc/group"
 root:x:0:
 EOF
 
-for i in $(ls "${initial_scripts}"/*); do
+for i in "${initial_scripts}"/*; do
+    [[ -e "$i" ]] || break
     echo "Running $(basename "${i}")" 1>&2
     "${i}" "${sysroot}"
 done

utils/find-rpath.sh
@@ -114,7 +114,7 @@ check_rpath() {
 	    needed_libs+=("${needed}")
 	done
 
-	rpath="$(objdump -x "${2}" | grep ${1} | sed "s/ *${1} *//")"
+	rpath="$(objdump -x "${2}" | grep "${1}" | sed "s/ *${1} *//")"
 	abspath="/$(realpath -m --no-symlinks --relative-to="${3}" "${2}")"
 	rpaths=()
 	IFS=:
@@ -162,7 +162,7 @@ find "${2}" -type f -not -name '*.debug' \
           '(' -perm -111 -o -name '*.so*' ')' \
           -print0 | (found_error=no; while read -r -d $'\0' file; do
 
-    read -n4 hdr <"${file}" || continue
+    read -rn4 hdr <"${file}" || continue
     if [ "$hdr" != "$(printf \\x7fELF)" ]; then
         continue
     fi

utils/generate-version
@@ -1,7 +1,7 @@
 #!/bin/sh
-timestamp=$(git log -1 --format="%at" ${CI_COMMIT_SHA1})
-cat >$1 <<EOF
+timestamp=$(git log -1 --format="%at" ${CI_COMMIT_SHA})
+cat >"$1" <<EOF
 variables:
     runtime-version: '$(git describe | sed 's/^freedesktop-sdk-//')'
-    runtime-version-date: $(date -d @$timestamp -Idate)
+    runtime-version-date: $(date -d @"$timestamp" -Idate)
 EOF

commit 767360050b059f1bf4c19c64adc31579a9b9a626

Feed ostree gpg options properly
This fixes a regression introduced in 17a43122. `gpg_opts` should be
expanded out side of quotation marks so that `ostree` can properly get
multiple --gpg-* options.

Without this fix, `bst track vm/minimal-ostree/image.bst` will fail
since the `ostree-repo` is not properly signed.

Fixes #1271

utils/update-repo.sh
@@ -89,7 +89,7 @@ ostree pull-local "${checkout}/ostree/repo" "${commit}"
 
 prev_commit="$(ostree rev-parse "${ref}" 2>/dev/null || true)"
 
-ostree commit "${gpg_opts[*]}" \
+ostree commit ${gpg_opts[*]} \
        --branch="${ref}" --tree=ref="${commit}" --skip-if-unchanged
 
 new_commit="$(ostree rev-parse "${ref}")"
@@ -103,6 +103,6 @@ if [ "${new_commit}" != "${prev_commit}" ]; then
 
     ostree summary \
            ${collection_id:+--add-metadata=ostree.deploy-collection-id='"'"${collection_id}"'"'} \
-           "${gpg_opts[*]}" \
+           ${gpg_opts[*]} \
            --update
 fi