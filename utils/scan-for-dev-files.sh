6eb27e4c7e6c8cb820c800b1fdec5bd1c77c21c8
Alessandro Bono <alessandro.bono369@gmail.com>
2022-01-18 12:20:28 +0000

    sasl: Include `*.so` symlinks in base package
    Otherwise sasl plugins cannot be find. When looking for plugins, sasl
    will try to load `*.so` symlinks. Don't mark them as development files.
    
    This is similar to what we did for the Yocto-based freedesktop-sdk-base[1]
    
    [1] https://github.com/flatpak/freedesktop-sdk-base/commit/eedcc1656b532174fdcfe9cdc0435a084f32add4

--- utils/scan-for-dev-files.sh
+++ utils/scan-for-dev-files.sh
@@ -15,6 +15,9 @@ while IFS= read -r -d '' file; do
     if [ "${basedir}" = engines-3 ]; then
 	continue
     fi
+    if [ "${basedir}" = sasl2 ]; then
+	continue
+    fi
     if [ "${basedir}" = vdpau ]; then
 	continue
     fi
